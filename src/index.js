ed4ed2c2a08388f1fc35900de3dba414f97aa27b
alex-krest <73220512+alex-krest@users.noreply.github.com>
2021-05-14 01:22:20 +0300

    Сделал разметку по шаблонам

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 import '/styles.css';
-
+import cards from './menu.json';
+import menuCardTml from './templates/menu-card.hbs';
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+console.log(menuCardTml(cards[0]));
+
+const cardsContainer = document.querySelector('.js-menu');
+const cardsMarkup = createMenuCardsMarkup(cards);
+
+cardsContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+function createMenuCardsMarkup(cards) {
+  return cards.map(menuCardTml).join(' ');
+}
